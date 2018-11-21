f219f9dcf1821a3df781bc6ea6717f5b99134095
hasanqazi <hasanahmedhaf@gmail.com>
2018-11-20 23:08:34 -0600

    Added tweets

--- tweetpage/views.py
+++ tweetpage/views.py
@@ -4,4 +4,4 @@ from .models import Tweet
 # Create your views here.
 def tweet_page(request):
   tweets = Tweet.objects.all().order_by('date')
-  return render(request, 'tweetpage/tweet_page.html')
+  return render(request, 'tweetpage/tweet_page.html',{'tweets':tweets})
