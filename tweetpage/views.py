b4af063a46a9df32511917c873e09436ded963de
hasanqazi <hasanahmedhaf@gmail.com>
2018-11-20 19:15:43 -0600

    Added models

--- tweetpage/views.py
+++ tweetpage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
 def tweet_page(request):
+  tweets = Tweet.objects.all().order_by('date')
   return render(request, 'tweetpage/tweet_page.html')
